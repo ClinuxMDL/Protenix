2c93c6d25a02472198a685c8cb3d4cedfe7130a7
MinshengHao <hmsh653@gmail.com>
2025-08-11 07:55:19 +0000

    modified:   .gitignore 	new file:   bash_infer_pxmini_volc.sh 	modified:   configs/configs_data.py 	modified:   finetune_pxmini_dev.sh 	modified:   finetune_pxmini_volc.sh 	new file:   finetune_pxmini_volc_clean16000.sh 	new file:   myfinetune_pxmini_dev.sh 	new file:   sample_idx.ipynb

--- myfinetune_pxmini_dev.sh
+++ myfinetune_pxmini_dev.sh
@@ -30,24 +30,21 @@ checkpoint_path="/voyager-hackathon/dataland/af3-dev/protenix_ckpts/protenix_min
 
 python3 ./runner/train.py \
 --model_name "protenix_mini_default_v0.5.0" \
---run_name clean_16140 \
+--run_name protenix_ft_md_new \
 --seed 42 \
 --base_dir ./output \
 --dtype bf16 \
 --project protenix_finetune_md \
 --use_wandb true \
 --diffusion_batch_size 48 \
---eval_interval 100 \
+--eval_interval 50 \
 --log_interval 5 \
---checkpoint_interval 500 \
+--checkpoint_interval 400 \
 --ema_decay 0.999 \
 --train_crop_size 384 \
---max_steps 1020 \
---lr 0.0018 \
+--max_steps 10000 \
 --load_checkpoint_path ${checkpoint_path} \
 --load_ema_checkpoint_path ${checkpoint_path} \
 --data.train_sets md0805_trainingset \
---data.test_sets md0805_testset1_sampled \
---data.md0805_testset1.base_info.max_n_token 500 \
---data.md0805_trainingset.base_info.indices_fpath /voyager-hackathon/home/hms/code/Protenix/mydata/md0805_training_ligand_prot_clean.csv \
---data.epoch_size 16000
+--data.test_sets md0805_testset1 \
+--data.md0805_testset1.base_info.max_n_token 500 
